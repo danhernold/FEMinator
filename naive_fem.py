97c03c1c73ee9706a7ea0867959e111242a0011e
Jonas Valfridsson <jonas@valfridsson.net>
2020-06-04 18:53:41 +0200

    naive fem

--- naive_fem.py
+++ naive_fem.py
@@ -4,45 +4,62 @@ from typing import Callable
 import matplotlib.pyplot as plt
 import sys
 
-nodes = np.array([
-    [0.0, 0.0],
-    [0.0, 0.25],
-    [0., 0.5],
-    [0.0, 0.75],
-    [0.0, 1.0],
-    [0.25, 0.0],
-    [0.25, 0.25],
-    [0.25, 0.5],
-    [0.25, 0.75],
-    [0.25, 1.0],
-    [0.5, 0.0],
-    [0.5, 0.25],
-    [0.5, 0.5],
-    [0.5, 0.75],
-    [0.5, 1.0],
-    [0.75, 0.0],
-    [0.75, 0.25],
-    [0.75, 0.5],
-    [0.75, 0.75],
-    [0.75, 1.0],
-    [1.0, 0.0],
-    [1.0, 0.25],
-    [1.0, 0.5],
-    [1.0, 0.75],
-    [1.0, 1.0],
-])
 
-domain_size = 200
-x_domain = np.linspace(0., 1., domain_size)
-y_domain = np.linspace(0., 1., domain_size)
+def example_problem():
+    side_nodes = 4
+    n_nodes = side_nodes * side_nodes
+    x_nodes = np.linspace(0, 1, side_nodes)
+    y_nodes = np.linspace(0, 1, side_nodes)
 
-domain = np.concatenate(
-    [_.reshape(-1, 1) for _ in np.meshgrid(x_domain, y_domain)], axis=1)
+    nodes = np.concatenate(
+        [_.reshape(-1, 1) for _ in np.meshgrid(y_nodes, x_nodes)], axis=-1)
 
-B = 1.0
-width = 4.0
+    print(nodes.shape)
 
-print("domain shape", domain.shape)
+    domain_size = 300
+    x_domain = np.linspace(0.0, 1.0, domain_size)
+    y_domain = np.linspace(0.0, 1.0, domain_size)
+
+    nodes_on_edge = np.array(
+        [i for i, node in enumerate(nodes) if (0 in node or 1 in node)])
+    nodes_on_domain = np.array(
+        [i for i, node in enumerate(nodes) if not (0 in node or 1 in node)])
+
+    domain = np.concatenate(
+        [_.reshape(-1, 1) for _ in np.meshgrid(x_domain, y_domain)], axis=1)
+
+    edge = np.concatenate([
+        np.concatenate([
+            np.linspace(0., 1, domain_size).reshape(-1, 1),
+            np.zeros(domain_size).reshape(-1, 1)
+        ],
+                       axis=1),
+        np.concatenate([
+            np.linspace(0., 1, domain_size).reshape(-1, 1),
+            np.ones(domain_size).reshape(-1, 1)
+        ],
+                       axis=1),
+        np.concatenate([
+            np.zeros(domain_size).reshape(-1, 1),
+            np.linspace(0., 1, domain_size).reshape(-1, 1)
+        ],
+                       axis=1),
+        np.concatenate([
+            np.ones(domain_size).reshape(-1, 1),
+            np.linspace(0., 1, domain_size).reshape(-1, 1)
+        ],
+                       axis=1)
+    ])
+
+    B = np.zeros(nodes.shape[0])
+    B[np.all(nodes == np.array([0.0, 0.0]), axis=1)] = 1.0
+    B[np.all(nodes == np.array([1.0, 0.0]), axis=1)] = 1.0
+    B[np.all(nodes == np.array([0.0, 1.0]), axis=1)] = 1.0
+    B[np.all(nodes == np.array([1.0, 1.0]), axis=1)] = 1.0
+
+    width = 0.25
+
+    return nodes_on_edge, nodes_on_domain, nodes, domain, edge, B, width
 
 
 @numba.jit(nopython=True, fastmath=True)
@@ -55,8 +72,13 @@ def linear_basis(x: np.ndarray, basis: np.ndarray, width: float):
     return np.maximum(1 - np.abs(x - basis).sum() / width, 0)
 
 
+basis = linear_basis
+
+
 @numba.jit(nopython=True, fastmath=True)
-def integrate(nodes: np.ndarray, domain: np.ndarray, B: float, width: float):
+def integrate(nodes_on_edge: np.ndarray, nodes_on_domain: np.ndarray,
+              nodes: np.ndarray, edge: np.ndarray, domain: np.ndarray,
+              B: np.ndarray, width: float):
     """Integrate over domain to attain K and F.
     
     K_ji = integral (\nabla \dot basis_i) * basis_j
@@ -64,39 +86,54 @@ def integrate(nodes: np.ndarray, domain: np.ndarray, B: float, width: float):
     """
     num_nodes, dim = nodes.shape
     num_domain, _ = domain.shape
+    num_edge, _ = edge.shape
 
     K = np.zeros((num_nodes, num_nodes))
     F = np.zeros(num_nodes)
 
-    epsilon = 1e-2
+    epsilon = 1e-6
     dx = np.array([epsilon, 0])
     dy = np.array([0, epsilon])
 
-    C = 0
+    integral_discretization_domain = num_domain 
+    integral_discretization_edge = num_edge 
 
-    integral_discretization_sz = domain_size * domain_size
+    # Integrate over domain
+    for j in nodes_on_domain:
+        for i in nodes_on_domain:
+
+            # Integrate over domain
+            for p in range(num_domain):
+                basis_j = basis(domain[p], nodes[j], width)
+                basis_i = basis(domain[p], nodes[i], width)
+
+                dx_basis_j = (basis(domain[p] + dx, nodes[j], width) -
+                              basis_j) / epsilon
+                dy_basis_j = (basis(domain[p] + dy, nodes[j], width) -
+                              basis_j) / epsilon
+
+                K[j, i] += (basis_i * (dx_basis_j + dy_basis_j) \
+                    / integral_discretization_domain)
+
+    # Integrate over edge
     for j in range(num_nodes):
         for i in range(num_nodes):
-            for p in range(num_domain):
-                basis_j = linear_basis(domain[p], nodes[j], width)
-                basis_i = linear_basis(domain[p], nodes[i], width)
+            for p in range(num_edge):
+                basis_j = basis(edge[p], nodes[j], width)
+                basis_i = basis(edge[p], nodes[i], width)
+                K[j, i] += -2*(basis_i * basis_j / integral_discretization_edge)
 
-                dx_basis_i = (linear_basis(domain[p] + dx, nodes[i], width) -
-                              basis_i) / epsilon
-                dy_basis_i = (linear_basis(domain[p] + dy, nodes[i], width) -
-                              basis_i) / epsilon
-
-                K[j, i] += (dx_basis_i + dy_basis_i) * basis_j\
-                    / integral_discretization_sz
-
+    # Integrate over domain
+    for j in range(num_nodes):
         for p in range(num_domain):
-            basis_j = linear_basis(domain[p], nodes[j], width)
-            F[j] += -(basis_j * B / integral_discretization_sz)
+            basis_j = basis(domain[p], nodes[j], width)
+            F[j] += -(basis_j * B[j] / integral_discretization_domain)
 
     return K, F
 
 
-def strain(x: np.ndarray, nodes: np.ndarray, u: np.ndarray, width: float):
+def calculate_stress(x: np.ndarray, nodes: np.ndarray, u: np.ndarray,
+                     width: float):
     """Calculate stress in a point given the solution u.
 
     stress(x) = sum_N u_i * basis_i(x)
@@ -109,28 +146,14 @@ def strain(x: np.ndarray, nodes: np.ndarray, u: np.ndarray, width: float):
     for i in range(num_samples):
         sample_stress = 0.0
         for j in range(num_nodes):
-            sample_stress += linear_basis(x[i], nodes[j], width) * u[j]
+            sample_stress += basis(x[i], nodes[j], width) * u[j]
 
         samples_stress.append(sample_stress)
 
     return np.array(samples_stress)
 
 
-K, F = integrate(nodes, domain, B, width)
-print("K", K)
-print("F", F)
-
-# Force bottom left to be 5.0
-F[0] = 5.0
-
-# Diagonalize K at 2
-K[0, :] = 0.0
-K[0, 0] = 1.0
-
-u = np.linalg.solve(K, F)
-
-
-def diff_hold(x, nodes, u, b, width):
+def verify_that_diffeq_holds(x, nodes, u, b, width):
     num_samples, _ = x.shape
     num_nodes, _ = nodes.shape
 
@@ -145,28 +168,33 @@ def diff_hold(x, nodes, u, b, width):
         stress_dy = 0.0
 
         for j in range(num_nodes):
-            stress += linear_basis(x[i], nodes[j], width) * u[j]
-            stress_dx += linear_basis(x[i] + dx, nodes[j], width) * u[j]
-            stress_dy += linear_basis(x[i] + dy, nodes[j], width) * u[j]
+            stress += basis(x[i], nodes[j], width) * u[j]
+            stress_dx += basis(x[i] + dx, nodes[j], width) * u[j]
+            stress_dy += basis(x[i] + dy, nodes[j], width) * u[j]
 
         divergence = ((stress_dx - stress) + (stress_dy - stress)) / epsilon
 
-        print("Divergence", divergence, "B", b, "Divergence + B",
-              divergence + b)
+        print("Divergence", divergence, "B", b[i], "Divergence + B",
+              divergence + b[i])
 
+
+nodes_on_edge, nodes_on_domain, nodes, domain, edge, B, width = example_problem(
+)
+K, F = integrate(nodes_on_edge, nodes_on_domain, nodes, edge, domain, B, width)
+
+u = np.linalg.solve(K, F)
 
 print()
 print("Proof that it works -- if divergence + B == 0 then the equation holds")
-diff_hold(nodes, nodes, u, B, width)
+verify_that_diffeq_holds(nodes, nodes, u, B, width)
 print(" ------------------ ")
 print()
 print()
 
-print("u", u)
-print("stress", strain(nodes, nodes, u, width))
-print("k @ u", K @ u)
+s = calculate_stress(domain, nodes, u, width)
 
-s = strain(domain, nodes, u, width)
+domain_size, _ = domain.shape
+domain_size = int(np.sqrt(domain_size))
 
 plt.figure(figsize=(20, 10))
 plt.imshow(s.reshape(domain_size, domain_size), origin="lower")
